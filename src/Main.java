084a34c2813f66c96010527eeba69f7b42281f23
sandy660 <sandrap.mejia@udea.edu.co>
2022-06-02 19:36:19 -0500

    Clase Principal y Método de desencriptación

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,16 @@
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
+        Security seguridad = new Security();
 
-        System.out.println("Hello World");
-        System.out.println("Otro cambio");
-
+        seguridad.addKey("Colombia");
+        System.out.println("Este es un ejemplo de RNF para la clase de Arquitectura de Software\n");
+        System.out.println("--------- Encriptado ----------");
+        String texto = seguridad.encriptar("Este es un ejemplo de RNF para la clase de Arquitectura de Software");
+        System.out.println(texto);
+        System.out.println();
+        System.out.println("--------- Desencriptado ----------");
+        System.out.println(seguridad.desencriptar(texto));
     }
 }
--- src/Security.java
+++ src/Security.java
@@ -1,10 +1,12 @@
 
 import javax.crypto.*;
 import javax.crypto.spec.SecretKeySpec;
+import java.io.IOException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 
+import Decoder.BASE64Decoder;
 import Decoder.BASE64Encoder;
 
 
@@ -50,5 +52,34 @@ public class Security {
         }
 
         return value;
+    }
+
+    /**
+     * Metodo para desencriptar un texto
+     * @param texto Texto encriptado
+     * @return String texto desencriptado
+     */
+    public String desencriptar(String texto){
+        String str="";
+        try {
+            byte[] value = new BASE64Decoder().decodeBuffer(texto);
+            cipher = Cipher.getInstance(algoritmo);
+            cipher.init(Cipher.DECRYPT_MODE,key);
+            byte[] cipherbytes= cipher.doFinal(value);
+            str = new String(cipherbytes);
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        } catch (NoSuchPaddingException e) {
+            System.err.println(e.getMessage());
+        } catch (NoSuchAlgorithmException e) {
+            System.err.println(e.getMessage());
+        } catch (InvalidKeyException e) {
+            System.err.println(e.getMessage());
+        } catch (IllegalBlockSizeException e) {
+            System.err.println(e.getMessage());
+        } catch (BadPaddingException e) {
+            System.err.println(e.getMessage());
+        }
+        return str;
     }
 }
